a703ac7d69b5dc300e13892ffa703e533468764a
Mina Mikhail <mina@fightingtheboss.com>
2015-06-22 16:27:18 -0400

    Install sass gem, split out npm install steps

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,22 +22,30 @@ echo installing Bundler
 gem install bundler -N >/dev/null 2>&1
 
 install Git git
-install SQLite sqlite3 libsqlite3-dev
+install SQLite sqlite3
 
-install PostgreSQL postgresql postgresql-contrib libpq-dev
+install PostgreSQL postgresql postgresql-contrib
 sudo -u postgres createuser --superuser vagrant
 
 install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
 
 # Install NodeJS v0.12.x
-curl -sL https://deb.nodesource.com/setup_0.12 | bash -
+echo 'Setting up NodeJS 0.12.x source'
+curl -sL https://deb.nodesource.com/setup_0.12 | bash - >/dev/null 2>&1
 install NodeJS nodejs
 
 # Needed for docs generation.
 update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
 
+echo installing Sass
+gem install sass -N >/dev/null 2>&1
+
 # Install Yeoman (+ dependencies) and Bitmaker Generator
-npm install -g yo grunt-cli bower generator-bitmaker
+echo 'Installing global npm dependencies'
+npm install -g yo
+npm install -g grunt-cli
+npm install -g bower
+npm install -g generator-bitmaker
 
 
 echo 'all set, rock on!'
